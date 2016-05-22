1f844b6064017f133ad68f6ab80b3ff5c1336621
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-05-22 22:31:32 +0000

    Fix finTech builder to deal with file structure.

--- haos_bash/build_a_fin_techy_haos.bash
+++ haos_bash/build_a_fin_techy_haos.bash
@@ -33,41 +33,47 @@ function setup_numpy() {
     sudo apt-get -y install python-numpy
 }
 
-function setup_temporary_fintech_workspace() {
+function create_fintech_workspace() {
+
+    local deep_learning_scripts_directory="finech_task_dependencies"
+    local fintech_task_runners="fintech_task_runners"
+    local data_source_directory="fintech_test_data"
+    local data_deposit_directory="data_runners_created"
+
     if [ -z "$1" ]
     then
         echo "no arg passed."
     else
         if make_directory_if_not_already_present "$1"
         then
-        echo "There is already a $1 directory. I don't know what to do!"
+            echo "There is already a $1 directory. I don't know what to do!"
         else
-        echo "created $1 directory."
-        cd "$1"
+            echo "created $1 directory."
+            cd "$1"
         
-:<<EOF
-EOF
-        # Get the files from GitHub repo : https://github.com/mnielsen/neural-networks-and-deep-learning
-        wget https://github.com/mnielsen/neural-networks-and-deep-learning/archive/master.zip
-        # Unzip the files.
-        unzip master.zip -d ./temp
-        local deep_learning_scripts_directory="finech_task_dependencies"
-        local fintech_task_runners="fintech_task_runners"
-        local data_source_directory="fintech_test_data"
-        local data_deposit_directory="data_runners_created"
-        # Make a directory for the tasks
-        mkdir "$deep_learning_scripts_directory"
-        mkdir "$fintech_task_runners"
-        mkdir "$data_source_directory"
-        
-        cp -rf ./temp/neural-networks-and-deep-learning-master/src/* "$deep_learning_scripts_directory/"
-        cp -rf ./temp/neural-networks-and-deep-learning-master/data/* "$data_source_directory"
-        
-        
-        setup_numpy
-        cd "$fintech_task_runners"
-        mkdir "$data_deposit_directory"
-        create_base_python_script "$data_deposit_directory"
+            # Get the files from GitHub repo : https://github.com/mnielsen/neural-networks-and-deep-learning
+            wget https://github.com/mnielsen/neural-networks-and-deep-learning/archive/master.zip
+            # Unzip the files.
+            unzip master.zip -d ./temp
+            # Make a directory for the tasks
+            mkdir "$deep_learning_scripts_directory"
+            mkdir "$fintech_task_runners"
+            mkdir "$data_source_directory"
+            
+            cp -rf ./temp/neural-networks-and-deep-learning-master/src/* "$deep_learning_scripts_directory/"
+            cp -rf ./temp/neural-networks-and-deep-learning-master/data/* "$data_source_directory"
+            rm -rf temp
+            
+            # This mods the mnist_loader.py to change where the loader looks for the test data.
+            local old_string="../data/mnist.pkl.gz"
+            local new_string="haos_work/fintech_test_data/data/mnist.pkl.gz"
+            sed -i -e 's,'"$old_string"','"$new_string"',g' ./"$data_source_directory"/mnist_loader.py
+
+            setup_numpy
+            
+            cd "$fintech_task_runners"
+            mkdir "$data_deposit_directory"
+            create_base_python_script "$data_deposit_directory"
         fi
 
     fi
@@ -75,20 +81,22 @@ EOF
 
 function create_base_python_script() {
 cat > fintec_test_script.py <<EOL
-import mnist_loader
-training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(sys.path[0]),'finech_task_dependencies'))
 
+import mnist_loader
 import network
+
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
 net = network.Network([784, 30, 10])
 
 net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
 
 EOL
-    
 }
 
-setup_temporary_fintech_workspace "haos_work"
+create_fintech_workspace "haos_work"
 
 
 :<<EOF
