fa79271d276d533e81a354e575edeea67de85e7d
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-05-23 03:24:52 +0000

    create vm builder.

---
+++ haos_bash/create_fintec_workshop.bash
@@ -0,0 +1,8 @@
+git config --global credential.helper 'cache --timeout=172800'
+
+git clone https://github.com/a-haos-at-ewha/38046_01_financial_technology.git public_fintec
+
+git clone https://gitlab.com/ewha_spring_2016/z_spring_2016_bash_toolset.git
+git clone https://gitlab.com/ewha_spring_2016/38046_01_financial_technology.git
+
+bash public_fintec/haos_bash/create_fintec_workshop.bash
