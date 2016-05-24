e769338bbcb05514df818408abcb4f5dd6b4ca0d
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-05-24 01:16:26 +0000

    Aw hell..

--- haos_bash/create_fintec_workshop.bash
+++ haos_bash/create_fintec_workshop.bash
@@ -13,15 +13,15 @@ function ask_user_to_continue() {
 cd $GOPATH
 
 git config --global credential.helper 'cache --timeout=172800'
-ask_user_to_continue
+#ask_user_to_continue
 
 git clone https://github.com/a-haos-at-ewha/38046_01_financial_technology.git public_fintec
-ask_user_to_continue
+#ask_user_to_continue
 
 git clone https://gitlab.com/ewha_spring_2016/38046_01_financial_technology.git private_fintec
-ask_user_to_continue
+#ask_user_to_continue
 
 git clone https://gitlab.com/ewha_spring_2016/z_spring_2016_bash_toolset.git
-ask_user_to_continue
+#ask_user_to_continue
 
 bash public_fintec/haos_bash/build_a_fin_techy_haos.bash
